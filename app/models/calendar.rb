bba137936d85af0ae14028381b11233af97697e7
mryou <mryou@maple.dti2.ne.jp>
2012-05-18 13:13:22 +0900

    とりあえず新規作成。入れ物だけ。

---
+++ app/models/calendar.rb
@@ -0,0 +1,3 @@
+class Calendar < ActiveRecord::Base
+  attr_accessible :calendar, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocalendarCore::Application.config.secret_token = '899ed709c2bc4be72450673b2239dc3e1b5141fed5ed2ee5e247dccdd851c0a05c415c39b5bfac8e6e0e029e35bf37ade894cff2c6339a80915af029afb3ee2a'
