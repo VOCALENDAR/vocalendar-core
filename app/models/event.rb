a67788ab618084fd13f193b2e16b9c6429e02be2
Tatsuki Sugiura <sugi@nemui.org>
2012-09-11 06:58:01 +0900

    Event schema updates.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ActiveRecord::Base
-  attr_accessible :calendar_id, :description, :etag, :event, :htmlLink, :location, :status, :summary, :kind, :colorId, :creatorEmail, :creatorDisplayName, :startDate, :startDateTime, :startTimeZone, :endDate, :endDateTime, :endTimeZone, :created, :updated
-  belongs_to :calendar
+  attr_accessible :g_calendar_id, :description, :etag, :g_html_ink,
+    :location, :status, :summary, :g_color_id, :g_creator_email,
+    :g_creator_display_name, :start_date, :start_datetime, 
+    :end_date, :end_datetime, :g_id, :recur_string, :recur_freq,
+    :recur_count, :recur_until, :recur_interval, :recur_wday,
+    :ical_uid, :tz_min, :country, :lang
   has_many :uris
 end
