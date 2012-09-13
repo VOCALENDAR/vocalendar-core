458a2f5427c8c6b429980670f2191990ca40ad88
Tatsuki Sugiura <sugi@nemui.org>
2012-09-13 16:59:55 +0900

    Stop to use STI on Calendar model.

---
+++ db/migrate/20120913075350_stop_to_use_st_ion_calendar.rb
@@ -0,0 +1,11 @@
+class StopToUseStIonCalendar < ActiveRecord::Migration
+  def up
+    rename_column :calendars, :type, :io_type
+    change_column :calendars, :io_type, :string, :null => false, :default => ''
+  end
+
+  def down
+    rename_column :calendars, :io_type, :type
+    change_column :calendars, :type, :string
+  end
+end
