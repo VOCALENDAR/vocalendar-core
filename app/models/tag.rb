91ee44c6eaf8328f7d18918b1617f27965fe9663
Tatsuki Sugiura <sugi@nemui.org>
2012-09-19 07:03:03 +0900

    Drop tags.is_category (not used)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,9 @@
 class Tag < ActiveRecord::Base
   default_scope order('name')
   has_and_belongs_to_many :events
+  has_and_belongs_to_many :calendars
 
-  attr_accessible :name, :is_category
+  attr_accessible :name
 
   validates :name, :presence => true, :format => {:with => /^\S+$/}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918191218) do
+ActiveRecord::Schema.define(:version => 20120918215514) do
 
   create_table "calendars", :force => true do |t|
     t.string   "name",                          :default => "", :null => false
@@ -96,13 +96,11 @@ ActiveRecord::Schema.define(:version => 20120918191218) do
   add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
 
   create_table "tags", :force => true do |t|
-    t.string   "name",        :default => "",    :null => false
-    t.boolean  "is_category", :default => false, :null => false
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.string   "name",       :default => "", :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
-  add_index "tags", ["is_category"], :name => "index_tags_on_is_category"
   add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
 
   create_table "uris", :force => true do |t|
