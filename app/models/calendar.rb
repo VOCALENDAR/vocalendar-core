b2f36cb96e6acec4145f9a65729695c28a9b1731
Tatsuki Sugiura <sugi@nemui.org>
2012-09-19 06:21:56 +0900

    Add cleanup task to delete published events when tag association changed.

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -44,21 +44,10 @@ class Calendar < ActiveRecord::Base
 
   def publish_events(opts = {})
     opts = {:force => false, :max => 2000}.merge opts
-    logger.info "Start event sync for calendar '#{name}' (##{id})"
+    logger.info "Start event publish for calendar '#{name}' (##{id})"
     count = 0
 
     self.update_attributes! :sync_started_at => DateTime.now
-
-    client = gapi_client
-    service = client.discovered_api('calendar', 'v3')
-
-    params = {
-      :headers => {'Content-Type' => 'application/json'},
-      :api_method => service.events.import,
-      :parameters => {
-        :calendarId => self.external_id,
-      }
-    }
 
     target_events = self.target_events.reorder('events.updated_at')
     self.latest_synced_item_updated_at && !opts[:force] and
@@ -69,13 +58,10 @@ class Calendar < ActiveRecord::Base
         logger.error "Sync skip! Event ##{event.id} don't have g_calendar_id & event.ical_uid"
         next
       end
-      params[:body] = event.to_exfmt(:google_v3, :tag_names_append => self.tag_names_append, :tag_names_remove => self.tag_names_remove).to_json
-      result = client.execute(params)
-      if result.status != 200
-        msg = "Error on import event (Status=#{result.status}): #{result.body}"
-        logger.error msg
-        raise msg
-      end
+      body = event.to_exfmt :google_v3,
+        :tag_names_append => self.tag_names_append,
+        :tag_names_remove => self.tag_names_remove
+      result = gapi_request :import, {}, body.to_json
       count += 1
       self.update_attributes! :latest_synced_item_updated_at => event.updated_at
       logger.info "Event '#{event.summary}' (##{event.id}) has been published to calendar '#{self.name}' (##{self.id}) successfully."
@@ -83,11 +69,28 @@ class Calendar < ActiveRecord::Base
     end
 
     self.update_attributes! :sync_finished_at => DateTime.now
-    logger.info "Event sync completed for calendar '#{name}' (##{id}): #{count} events has been updated (#{DateTime.now.to_i - self.sync_started_at.to_i} secs)."
+    logger.info "Event publish completed for calendar '#{name}' (##{id}): #{count} events has been updated (#{DateTime.now.to_f - self.sync_started_at.to_f} secs)."
   end
 
   def cleanup_published_events(opts = {})
-    
+    logger.info "Start published event cleanup for calendar '#{name}' (##{id})"
+    start_time = DateTime.now.to_f
+    remote_eids = []
+    self.gapi_list_each_page(:showDeleted => false) do |result|
+      result.data["items"] or break
+      remote_eids += result.data.items.map { |e| e.id }
+    end
+    local_eids = []
+    target_events.each do |e|
+      e.g_id.blank? and next
+      local_eids << e.g_id
+    end
+    delete_eids = remote_eids - local_eids
+    delete_eids.each do |eid|
+      logger.info "Delete event from calendar '#{self.name}' (##{self.id}): google event ID=#{eid}"
+      gapi_request :delete, {:eventId => eid}
+    end
+    logger.info "Event cleanup completed: #{delete_eids.size} events has been deleted (#{DateTime.now.to_f - start_time} secs)"
   end
 
   def fetch_events(opts = {})
@@ -119,7 +122,7 @@ class Calendar < ActiveRecord::Base
           self.update_attributes! :latest_synced_item_updated_at => eitem["updated"]
           opts[:max] <= count and break
         rescue => e
-          logger.error "Failed to sync event: #{e.class.name}: #{e.to_s} (#{e.backtrace.first})"
+          logger.error "Failed to sync event: #{e.class.name}: #{e.to_s} (#{e.backtrace.join(' > ')})"
           logger.error "Failed item: #{eitem.inspect}"
           raise e
         end
@@ -127,39 +130,28 @@ class Calendar < ActiveRecord::Base
     end
 
     self.update_attributes! :sync_finished_at => DateTime.now
-    logger.info "Event sync completed for calendar '#{name}' (##{id}): #{count} events has been updated (#{DateTime.now.to_i - self.sync_started_at.to_i} secs)."
+    logger.info "Event sync completed for calendar '#{name}' (##{id}): #{count} events has been updated (#{DateTime.now.to_f - self.sync_started_at.to_f} secs)."
   end
 
-  def gapi_list_each_page(query_params = {}, &block)
-    client = gapi_client
-    service = client.discovered_api('calendar', 'v3')
+  def gapi_list_each_page(params = {}, &block)
     params = {
-      :headers => {'Content-Type' => 'application/json'},
-      :api_method => service.events.list,
-      :parameters => {
-        :calendarId => self.external_id,
-        :singleEvents => false,
-        :showDeleted => true,
-        :orderBy => 'updated',
-      }.merge(query_params)
-    }
+      :singleEvents => false,
+      :showDeleted => true,
+      :orderBy => 'updated',
+    }.merge(params)
 
     while true
       logger.debug "Getting event list via Google API: #{params.inspect}"
-      result = client.execute(params)
-      if result.status != 200
-        msg = "Error on getting event list (Status=#{result.status}): #{result.body}"
-        logger.error msg
-        #logger.error result.inspect
-        raise msg
-      end
-      logger.debug 'Get event list successfully'
-
+      result = gapi_request :list, params
       yield result
-
       result.next_page_token or break
-      params[:parameters][:pageToken] = result.next_page_token
+      params[:pageToken] = result.next_page_token
     end
+  end
+
+  alias_method :_gapi_request, :gapi_request
+  def gapi_request(method, params = {}, body = nil)
+    _gapi_request(method, {:calendarId => self.external_id}.merge(params), body)
   end
 
   private
