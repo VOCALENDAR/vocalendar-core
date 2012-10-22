c8239bc19a61f6ef39fce3c225033929f3c7e0db
mryou <ryou@maple.dti2.ne.jp>
2012-10-22 18:54:54 +0900

    必要項目は全部仮で入れた。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,7 +19,7 @@ class EventsController < ApplicationController
         # Change to Google JSON
         # TOP level
         calendarid = @events == nil ? 'null' : @events[0].g_calendar_id
-        @calendars = Calendar.joins('inner join calendar on calendar_id = ').where('' => @events[0].calendarid)
+        @calendars = Calendar.where('external_id' => calendarid)
 
         json = {:version => '1.0', :encoding => 'UTF-8'}
         # TODO feed情報
@@ -29,13 +29,31 @@ class EventsController < ApplicationController
             'xmlns$openSearch' => 'http://a9.com/-/spec/opensearchrss/1.0/',
             'xmlns$gCal' => 'http://schemas.google.com/gCal/2005',
             'xmlns$gd' => 'http://schemas.google.com/g/2005',
-            :id => 'http://www.google.com/calendar/feeds/' + @calendar[0].external_id + '/public/full',
-            :updated => {:$t => @calendar[0].latest_synced_item_updated_at},
+            :id => 'http://www.google.com/calendar/feeds/' + @calendars[0].external_id + '/public/full',
+            :updated => {:$t => @calendars[0].latest_synced_item_updated_at},
             :category => [{ :scheme => 'http://schemas.google.com/g/2005#kind',
                             :term => 'http://schemas.google.com/g/2005#event'
                           }],
-            :title => { :$t => @calendar[0].name, :type => 'text'},
+            :title => { :$t => @calendars[0].name, :type => 'text'},
             :subtitle => { :$t => '', :type => 'text'},
+            :link => [
+              { :rel => 'alternate', :type => "text/html", :href => 'https://www.google.com/calendar/embed?src=' + calendarid},
+              { :rel => 'http://schemas.google.com/g/2005#feed', :type => 'application/atom+xml', :href => 'https://www.google.com/calendar/feeds/' + calendarid  + '/public/full'},
+              { :rel => 'http://schemas.google.com/g/2005#batch', :type => 'application/atom+xml', :href => 'https://www.google.com/calendar/feeds/' + calendarid  + '/public/full/batch'},
+              # TODO パラメータ化
+              { :rel => 'self', :type => 'application/atom+xml', :href =>'https://www.google.com/calendar/feeds/' + calendarid  + '/public/full?alt=json-in-script&q=cd%E3%80%80&start-index=151&max-results=25&singleevents=true&start-max=2015-09-26T16%3A04%3A16Z&sortorder=ascending&orderby=starttime'},
+              { :rel => 'previous', :type => 'application/atom+xml', :href =>'https://www.google.com/calendar/' + calendarid  + '/public/full?alt=json-in-script&q=cd%E3%80%80&start-index=126&max-results=25&singleevents=true&start-max=2015-09-26T16%3A04%3A16Z&sortorder=ascending&orderby=starttime'},
+              { :rel => 'next', :type => 'application/atom+xml', :href =>'https://www.google.com/calendar/feeds/' + calendarid  + '/public/full?alt=json-in-script&q=cd%E3%80%80&start-index=176&max-results=25&singleevents=true&start-max=2015-09-26T16%3A04%3A16Z&sortorder=ascending&orderby=starttime'}
+            ],
+            :author => [ { :name => { :$t => 'editor.vocalendar@gmail.com'}, :email => { :$t => 'editor.vocalendar@gmail.com'}}],
+            :generator => { :$t => 'Google Calendar', :version => '1.0', :uri => 'http://www.google.com/calendar'},
+            'openSearch$totalResults' => { :$t => @events.size},
+            # TODO パラメータ化
+            'openSearch$startIndex' => { :$t => 0},
+            'openSearch$itemsPerPage' => { :$t => 25},
+            'gCal$timezone' => { :value => 'Asia/Tokyo'},
+            'gCal$timesCleaned' => { :value => 0},
+            'gd$where' => {:valueString => ''},
 
 
 
@@ -44,7 +62,7 @@ class EventsController < ApplicationController
         entry = []
         @events.each_with_index { |event, i|
 
-          feedurl = 'http://www.google.com/calendar/feeds/' + event.g_calendar_id + '/public/full' + event.g_id
+          feedurl = 'http://www.google.com/calendar/feeds/' + event.g_calendar_id + '/public/full/' + event.g_id
           entry[i] = {
                       :id => {:$t => feedurl},
                       :published => {:$t => event.created_at},
@@ -62,12 +80,12 @@ class EventsController < ApplicationController
                       'gd$comments' => {'gd$feedLink' => {:href => feedurl + '/comments'}},
                       'gd$eventStatus' => { :value => 'http://schemas.google.com/g/2005#event.' + event.status },
                       'gd$where' => [{:valueString => event.location}],
-                      'gd$who' => [:email => event.g_creator],
+                      'gd$who' => [:email => event.g_creator_email],
                       :rel => 'http://schemas.google.com/g/2005#event.organizer',
                       :valueString => 'VOCALENDAR # メイン',
                       'gd$when' => [
                                   {:endtime => event.end_datetime},
-                                  {:startTime => event.start_datestarS}
+                                  {:startTime => event.start_datetime}
                       ],
 
                       # original columns
