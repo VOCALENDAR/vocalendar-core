d562e921e65e0eadae54dfab75cb2ca2a102ffee
Tatsuki Sugiura <sugi@nemui.org>
2012-11-08 20:23:43 +0900

    Change Google API client info loader.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,18 +1,12 @@
-Rails.configuration.google_client_configured = false
-
-begin
-  gsetting = YAML.load_file "#{Rails.root}/config/google-api.yml"
-  gsetting["client_id"].blank?  and raise Errno::ENOENT
-  gsetting["api_secret"].blank? and raise Errno::ENOENT
+if Rails.configuration.google_client_configured
+  client_id     = Rails.configuration.google_client_id
+  client_secret = Rails.configuration.google_client_secret
 
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, gsetting["client_id"], gsetting["api_secret"], {
+    provider :google_oauth2, client_id, client_secret, {
       access_type: 'offline',
       scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
       # redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
     }
-    Rails.configuration.google_client_configured = true
   end
-rescue Errno::ENOENT
-  # ignore
 end
