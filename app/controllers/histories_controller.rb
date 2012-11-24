d575511ec44ae781cdf1414d27af6c24e836b5a2
Tatsuki Sugiura <sugi@nemui.org>
2012-11-25 04:49:44 +0900

    Improve history listing.

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,10 +1,14 @@
 class HistoriesController < ApplicationController
   load_and_authorize_resource
   def index
-    @histories = @histories.page(params[:page]).per(150)
-    params[:target] and
-      @histories = @histories.where(:target => params[:params])
-    params[:user_id] and
+    @histories = @histories.page(params[:page]).per(100)
+    unless params[:target].blank?
+      @histories = @histories.where(:target => params[:target])
+      params[:id].blank? or
+        @histories = @histories.where(:target_id => params[:id])
+    end
+
+    params[:user_id].blank? or
       @histories = @histories.where(:user_id => params[:user_id])
 
     respond_with(@histories)
