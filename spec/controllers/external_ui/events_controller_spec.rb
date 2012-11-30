8a892c6f5e730f965e49a0ee8753a8685aa77296
Tatsuki Sugiura <sugi@nemui.org>
2012-12-01 03:08:15 +0900

    Spec cleanup.

--- spec/controllers/external_ui/events_controller_spec.rb
+++ spec/controllers/external_ui/events_controller_spec.rb
@@ -8,18 +8,16 @@ describe ExternalUi::EventsController do
         allday: true,
         country: "jp",
         description: "desc\nription",
-        end_date: Date.parse("2003-02-27"),
-        end_datetime: DateTime.parse("2003-02-27T09:00:00+09:00"),
+        end_datetime: DateTime.now + 1.day,
         etag: "bt6uG7OvVvCre70u9H5QXyrDIXY/Q05qcHhNSGJKaEVBQUFBQUFBQUFBQT09",
         g_calendar_id: "pcg8ct8ulj96ptvqhllgcc181o@group.calendar.google.com",
-        g_creator_email: "mizuki.a.ryou@gmail.com",
+        g_creator_email: "testuser@gmail.com",
         g_html_link: "https://www.google.com/calendar/event?eid=M25vN3U1Z2U3NW5zMGNvOWphN21nOWl0anMgcGNnOGN0OHVsajk2cHR2cWhsbGdjYzE4MW9AZw",
         g_id: "3no7u5ge75ns0co9ja7mg9itjs",
         ical_uid: "3no7u5ge75ns0co9ja7mg9itjs@google.com",
         lang: "ja",
         recur_string: "",
-        start_date: Date.parse("2003-02-26"),
-        start_datetime: DateTime.parse("2003-02-26T09:00:00+09:00"),
+        start_datetime: DateTime.now,
         status: "confirmed",
         summary: "YAMAHA VOCALOID",
       }
@@ -41,7 +39,10 @@ describe ExternalUi::EventsController do
     it "ignore blank search" do
       get :index, :q => ''
       response.should be_success
-      assigns(:events).should have(1).items
+      ret_with_blank_query = assigns(:events)
+      get :index
+      response.should be_success
+      assigns(:events).should eq ret_with_blank_query
     end
 
   end
