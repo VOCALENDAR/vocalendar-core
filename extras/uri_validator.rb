636aeae86241a2a9f2b600c590e2c7c0f57d407e
Tatsuki Sugiura <sugi@nemui.org>
2012-12-04 02:44:33 +0900

    More strict URL check.

--- extras/uri_validator.rb
+++ extras/uri_validator.rb
@@ -1,11 +1,12 @@
 class UriValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
+    err_class = Addressable::URI::InvalidURIError
     begin
+      #value =~ %r{^https?://} or raise err_class
       uri = Addressable::URI.parse(value)
-      if !["http","https"].include?(uri.scheme)
-        raise Addressable::URI::InvalidURIError
-      end
-    rescue Addressable::URI::InvalidURIError
+      ["http","https"].include?(uri.scheme) or raise err_class
+      uri.host.to_s.include?(".") or raise err_class
+    rescue err_class
       object.errors[attribute] << (options[:message] || I18n.t("errors.messages.invalid_uri", default: " is invalid URI"))
     end
   end
