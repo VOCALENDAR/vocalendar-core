0873ce711492c2611255ebd98532763ee41ff481
Tatsuki Sugiura <sugi@nemui.org>
2012-12-04 17:46:06 +0900

    Improve access recording. (use request.remote_ip)

--- app/controllers/ex_links_controller.rb
+++ app/controllers/ex_links_controller.rb
@@ -46,8 +46,8 @@ class ExLinksController < ApplicationController
     begin
       ExLinkAccess.create({
                             :ex_link_id => @ex_link.id,
-                            :ipaddr => request.env['REMOTE_ADDR'],
-                            :user_agent => request.env['HTTP_USER_AGENT'].to_s[0..100],
+                            :ipaddr => request.remote_ip,
+                            :user_agent => request.env['HTTP_USER_AGENT'].to_s[0..200],
                           }, :without_protection => true)
     rescue Exception => e
       logger.error "Failed to save access #{e.class.name}: #{e.message}"
