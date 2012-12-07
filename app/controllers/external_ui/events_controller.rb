c6303d5b3ff05bcff6e0e406887c15658a396d5e
Tatsuki Sugiura <sugi@nemui.org>
2012-12-07 23:04:28 +0900

    CD release view sample.

--- app/controllers/external_ui/events_controller.rb
+++ app/controllers/external_ui/events_controller.rb
@@ -13,6 +13,16 @@ class ExternalUi::EventsController < ApplicationController
     respond_with @events, :layout => detect_layout
   end
 
+  def cd_releases
+    load_cd_releases
+    respond_with @events, :layout => detect_layout
+  end
+
+  def cd_releases_body
+    load_cd_releases
+    respond_with @events, :layout => nil
+  end
+
   def show
     ae = Event.active
     if params[:id]
@@ -30,6 +40,28 @@ class ExternalUi::EventsController < ApplicationController
 
   private
   def detect_layout
+    request.xhr? and false
     params[:embed].blank? ? "mainsite_dummy" : "external_ui_embed"
   end
+
+  def load_cd_releases
+    pivot = nil
+    begin
+      pivot = Date.parse(params[:pivot])
+    rescue
+      pivot = Date.today
+    end
+    start_date = pivot - 1.month
+    start_date = Time.new(start_date.year, start_date.month, 1, 0, 0, 0).to_datetime
+    end_date   = pivot + 2.month
+    end_date   = Time.new(end_date.year, end_date.month, 1, 0, 0, 0).to_datetime
+    @events = Event.active.reorder('start_datetime').
+      where("start_datetime >= ?", start_date).
+      where("start_datetime < ?",  end_date).
+      joins(:tags).where("tags.name" => "CD")
+    @start_date = start_date
+    @end_date   = end_date
+    @pivot_date = pivot
+  end
+
 end
